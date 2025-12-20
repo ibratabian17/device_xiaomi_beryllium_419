ed859758fbe1b10b2bba279d3df87c1f2f879325
ibratabian17 <ibratabian@gmail.com>
2025-12-20 13:29:27 +0700

    beryllium: PixelOS Bringup

--- custom_beryllium.mk
+++ custom_beryllium.mk
@@ -7,11 +7,11 @@
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := custom_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
